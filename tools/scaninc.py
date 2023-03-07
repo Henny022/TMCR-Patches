bd019d714e253b77701fbfdc44d8f8e44cc18aab
Henny022 <git@henny022.de>
2023-03-08 00:33:46 +0100

    :sparkles: [scaninc] properly handle subdirs and incbin

--- tools/scaninc.py
+++ tools/scaninc.py
@@ -1,29 +1,34 @@
 import re
 import sys
-import os.path
+from pathlib import Path
 
 
-pattern = re.compile(r'#include "(?P<file>.+?)"')
-def scan(filename):
-    if not os.path.exists(filename):
+pattern = re.compile(r'#inc(lude|bin) "(?P<file>.+?)"')
+def scan(path):
+    if not path.exists():
         return []
-    with open(filename) as file:
+    with open(path) as file:
         content = file.read()
-    return pattern.findall(content)
+    return [
+        path.parent / file
+        for mode, file in pattern.findall(content)
+        if mode != 'bin'
+    ]
     
 
 def main(argv=None):
     argv = argv or sys.argv[1:]
     if len(argv) < 1:
-        print(f'usage: {sys.argv[0]} FILES...')
-    W = [x for x in argv]
+        print(f'usage: {sys.argv[0]} FILES...', file=sys.stderr)
+        exit(1)
+    W = [Path(x) for x in argv]
     P = []
     while W:
         file, W = W[0], W[1:]
         P.append(file)
         t = scan(file)
         W += [x for x in t if x not in P]
-    print('\n'.join([x for x in P if x not in argv]))
+    print('\n'.join([str(x) for x in P if x not in argv]))
 
 
 if __name__ == "__main__":
