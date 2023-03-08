5abec00131f44ee895114923725486cba6265366
Henny022 <git@henny022.de>
2023-03-08 19:32:38 +0100

    :bug: [scaninc] fix various small bugs

--- tools/scaninc.py
+++ tools/scaninc.py
@@ -1,18 +1,18 @@
+#!/usr/bin/env python
 import re
 import sys
 from pathlib import Path
 
 
-pattern = re.compile(r'#inc(lude|bin) "(?P<file>.+?)"')
+pattern = re.compile(r'^#inc(lude|bin) "(?P<file>.+?)"', re.MULTILINE)
 def scan(path):
     if not path.exists():
         return []
     with open(path) as file:
         content = file.read()
     return [
-        path.parent / file
+        (mode, path.parent / file)
         for mode, file in pattern.findall(content)
-        if mode != 'bin'
     ]
     
 
@@ -21,14 +21,15 @@ def main(argv=None):
     if len(argv) < 1:
         print(f'usage: {sys.argv[0]} FILES...', file=sys.stderr)
         exit(1)
-    W = [Path(x) for x in argv]
+    A = [Path(x) for x in argv]
+    W = [x for x in A]
     P = []
     while W:
         file, W = W[0], W[1:]
-        P.append(file)
         t = scan(file)
-        W += [x for x in t if x not in P]
-    print('\n'.join([str(x) for x in P if x not in argv]))
+        W += [x for mode, x in t if x not in P and mode != 'bin']
+        P += [x for mode, x in t if x not in P]
+    print('\n'.join([str(x) for x in P]))
 
 
 if __name__ == "__main__":
