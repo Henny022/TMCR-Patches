2d49689f106584e8318e8f3dcd8b74ba1031bc8c
Henny022 <git@henny022.de>
2023-03-27 16:10:41 +0200

    :sparkles: new game patch
    items, flags, location

---
+++ new_game.c
@@ -0,0 +1,44 @@
+#include <save.h>
+#include <common.h>
+#include <flags.h>
+
+extern const u32 n_starting_items;
+extern const u8 starting_items[];
+extern const u32 n_starting_flags;
+extern const u16 starting_flags[];
+extern const u8 starting_area;
+extern const u8 starting_room;
+extern const u8 starting_anim_state;
+extern const struct{
+    s16 x;
+    s16 y;
+} starting_pos;
+
+u32 FinalizeSave(void) {
+    if (gSave.invalid || gSave.initialized != 1) {
+        gSave.invalid = 0;
+        gSave.initialized = 1;
+        gSave.global_progress = 1;
+        gSave.stats.health = 24;
+        gSave.stats.maxHealth = 24;
+        gSave.saved_status.area_next = starting_area;
+        gSave.saved_status.room_next = starting_room;
+        gSave.saved_status.start_anim = starting_anim_state;
+        gSave.saved_status.spawn_type = PL_SPAWN_DEFAULT;
+        gSave.saved_status.layer = 1;
+        gSave.saved_status.start_pos_x = starting_pos.x;
+        gSave.saved_status.start_pos_y = starting_pos.y;
+        for (u32 i = 0; i < n_starting_flags; i++)
+        {
+            SetGlobalFlag(starting_flags[i]);
+        }
+        for (u32 i = 0; i < n_starting_items; i++)
+        {
+            SetInventoryValue(starting_items[i], 1);
+        }
+    }
+    if (gSave.name[0] == 0) {
+        MemCopy(gUnk_0811E470, &gSave.name, FILENAME_LENGTH - 1);
+    }
+    return 1;
+}
