8788554dadb4555f76c90849dc7a1d5086428ac0
Henny022 <git@henny022.de>
2025-05-04 15:46:31 +0200

    fix ezlo tiggers not setting flags
    Co-authored-by: Ibot02 <Ibot02@users.noreply.github.com>

--- qol/shutupezlo.c
+++ qol/shutupezlo.c
@@ -1,7 +1,37 @@
 #include <manager.h>
 #include <manager/ezloHintManager.h>
+#include <player.h>
+#include <flags.h>
+#include <asm.h>
 
-void EzloHintManager_Init(EzloHintManager* this) {
-    (void) this;
-    DeleteThisEntity();
+extern bool32 PlayerStateValid(EzloHintManager* this);
+extern void sub_080585DC(EzloHintManager* this);
+
+void EzloHintManager_Action2(EzloHintManager* this) {
+    if (gPlayerState.flags & PL_NO_CAP)
+        return;
+    if (!CheckPlayerInRegion(this->x, this->y, this->rx, this->ry))
+        return;
+    switch (super->type) {
+        case 1:
+            return;
+        case 2:
+            sub_080585DC(this);
+            if ((gPlayerState.flags & PL_MINISH) == 0)
+                return;
+            // fallthrough
+        case 0:
+        default:
+            sub_080585DC(this);
+            if (!PlayerStateValid(this))
+                return;
+            SetFlag(this->flag1);
+            DeleteThisEntity();
+            return;
+        case 3:
+            super->action = 4;
+            super->subAction = 0;
+            SetNextAreaHint(this->msg_idx);
+            return;
+    }
 }
